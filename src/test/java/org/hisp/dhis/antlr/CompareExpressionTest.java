a3e393984ffce3ea6493da462276b2f7d5d70a15
Enrico Colasante <enrico.colasante@gmail.com>
2020-03-06 10:01:00 +0100

    Fix compare for string and doubles

--- src/test/java/org/hisp/dhis/antlr/CompareExpressionTest.java
+++ src/test/java/org/hisp/dhis/antlr/CompareExpressionTest.java
@@ -13,6 +13,17 @@ public class CompareExpressionTest
     private TestExpressionVisitor visitor = new TestExpressionVisitor();
 
     @Test
+    public void testGreaterForDifferentTypes() {
+//        assertEquals( true, evaluate( "'2' > 1" ) );
+//        assertEquals( false, evaluate( "1 > '2'" ) );
+//        assertEquals( false, evaluate( "'1' > '2'" ) );
+//        assertEquals( true, evaluate( "'1' > '-2'" ) );
+        assertEquals( true, evaluate( "'-1' > '-2'" ) );
+        assertEquals( false, evaluate( "'-1' > '2'" ) );
+        assertEquals( false, evaluate( "'2' > ( 1 + 1 )" ) );
+    }
+
+    @Test
     public void testGreater() {
         assertEquals( true, evaluate( "2 > 1" ) );
         assertEquals( false, evaluate( "1 > 2" ) );
